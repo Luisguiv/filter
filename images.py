fe6e6b05d3963351a3e6155eb7f022765a0eeded
Luisguiv <luisguivos@hotmail.com>
2023-10-05 21:05:38 -0300

    last

--- images.py
+++ images.py
@@ -8,9 +8,9 @@ img_rocket = data.rocket()
 img_text = data.text()
 
 # Do diretorio
-img_valve = cv2.imread('./images/valve.png')
-img_spider = cv2.imread('./images/spider.jpeg')
-img_lena = cv2.imread('./images/lena.png')
+img_valve = cv2.imread('./images/valve.jpg')
+img_spider = cv2.imread('./images/spider.jpg')
+img_lena = cv2.imread('./images/lena.jpg')
 img_woman = cv2.imread('./images/woman.jpg') # Ja em grayscale
-img_cow = cv2.imread('./images/cow.png')
+img_cow = cv2.imread('./images/cow.jpg')
 img_coin = cv2.imread('./images/coin.jpg')
